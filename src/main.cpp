94e1c500d314a8bfa7f3f028854364705a8c0d7b
xXenvy <quentisxd@gmail.com>
2024-07-01 19:28:21 +0200

    feat: add `init` command logic

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,14 @@
 ﻿#include <iostream>
+#include <fstream>
 #include "cli.h"
 
 constexpr char const* version = "0.0.1";
+
+void create_init_file() {
+	std::ofstream initfile("main.zk");
+	initfile << "def main() {\n\n}\n";
+	std::cout << "Successfully created a new main.zk file." << std::endl;
+}
 
 int main(int argc, char* argv[]) {
 	CLI cli(std::vector<std::string>(argv + 1, argv + argc));
@@ -17,6 +24,10 @@ int main(int argc, char* argv[]) {
 	}
 	if (cli.args.version) {
 		std::cout << "Version: " << version << std::endl;
+		return 0;
+	}
+	if (cli.args.init) {
+		create_init_file();
 		return 0;
 	}
 }
